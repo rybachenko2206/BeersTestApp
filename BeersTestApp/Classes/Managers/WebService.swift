b0fb84342e085185a02621dde71b9af085316797
Roman Rybachenko <rrybachenko@icloud.com>
2020-01-24 22:00:07 +0200

    initial commit

---
+++ BeersTestApp/Classes/Managers/WebService.swift
@@ -0,0 +1,15 @@
+//
+//  WebService.swift
+//  BeersTestApp
+//
+//  Created by Roman Rybachenko on 24.01.2020.
+//  Copyright © 2020 Roman Rybachenko. All rights reserved.
+//
+
+import Foundation
+
+class WebService {
+    static let shared = WebService()
+    
+    private init() { }
+}
---
+++ BeersTestApp/Classes/ViewControllers/BeersViewController.swift
@@ -0,0 +1,16 @@
+//
+//  BeersViewController.swift
+//  BeersTestApp
+//
+//  Created by Roman Rybachenko on 24.01.2020.
+//  Copyright © 2020 Roman Rybachenko. All rights reserved.
+//
+
+import UIKit
+
+class BeersViewController: UIViewController, Storyboardable {
+
+    static var storyboardName: Storyboard {
+        return .beers
+    }
+}
